2c7f6d43bc4bc702db1782c8f10726f290c9ed77
CrackGames777 <diegogames.crack@gmail.com>
2026-09-24 16:27:17 -0300

    Agregando archivo de configuracion para Render

--- BackendApi/obj/Debug/net9.0/BackendApi.AssemblyInfo.cs
+++ BackendApi/obj/Debug/net9.0/BackendApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c255cb37a498ce9d4f05ddc8ec1655d468b70f1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
